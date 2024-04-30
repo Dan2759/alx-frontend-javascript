3528217bff5cf91b2abafc644dcdecf154433c25
Dan2759 <kwizeradan.kd27@gmail.com>
2024-04-30 23:11:12 +0000

    ES6_data_manipulation

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buf = new ArrayBuffer(length);
-  const val = new DataView(arr);
-
-  try {
-    val.setInt8(position, value);
-  } catch (e) {
-    throw Error('Position outside range');
+  if (position >= length) {
+    throw new Error('Position outside range');
   }
+
+  const buffer = new ArrayBuffer(length);
+  const val = new DataView(buffer);
+  val.setInt8(position, value);
   return val;
 }
